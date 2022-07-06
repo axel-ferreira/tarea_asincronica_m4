575a49078b7b6f7873f16c099bfac7ce257cef39
axel ferreira <axeljoaopabloferreira@gmail.com>
2022-07-05 23:25:29 -0400

    primer commit de la evalucaion

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputPrin = document.querySelector (".input")
+const btnAgre = document.querySelector ("boton-agregar")
+const container = document.querySelector ("container")
+
+class item {
+    
+}
